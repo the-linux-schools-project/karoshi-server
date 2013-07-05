63ad2b5f079fad826844619899c3140b9b99c043
Paul Sharrad <mpsharrad@karoshi.org.uk>
2013-07-05 15:31:13 +0100

    Default windows client profile updated
    This is a new version of the default windows profile to speed up windows clients logging in.
    
    This also fixed problems with the profileuser account.

--- serversetup/pdc/profiles/defaultprofile/Application Data/Adobe/Acrobat/6.0/Updater/udstore.js
+++
@@ -1,1 +0,0 @@
-try{eval((new String("updater.store.perUser=({nextCheckDate:\"Sun, 02 Jan 2005 16:04:06 GMT\", viewedMessages:{}})")).valueOf());}catch(e){updater.console.println("eval store exception:"+e);}
--- serversetup/pdc/profiles/defaultprofile/Application Data/Mozilla/Firefox/Profiles/default.kg7/user.js
+++
@@ -1,5 +0,0 @@
-user_pref("browser.bookmarks.file","g:\favourites\bookmarks");
-user_pref("capability.policy.policynames", "allowclipboard");
-user_pref("capability.policy.allowclipboard.sites", "http://colossus.internal");
-user_pref("capability.policy.allowclipboard.Clipboard.cutcopy", "allAccess");
-user_pref("capability.policy.allowclipboard.Clipboard.paste", "allAccess");
