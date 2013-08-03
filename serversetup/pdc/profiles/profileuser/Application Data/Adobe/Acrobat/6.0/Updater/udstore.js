844ba00540dab09ca75b585c792286bd2a2b9ca0
Paul Sharrad <mpsharrad@karoshi.org.uk>
2013-08-03 17:38:02 +0100

    setupsamba - profileuser profile added back in.

---
+++ serversetup/pdc/profiles/profileuser/Application Data/Adobe/Acrobat/6.0/Updater/udstore.js
@@ -0,0 +1,1 @@
+try{eval((new String("updater.store.perUser=({nextCheckDate:\"Thu, 29 Apr 2004 02:52:26 GMT\", viewedMessages:{}})")).valueOf());}catch(e){updater.console.println("eval store exception:"+e);}
