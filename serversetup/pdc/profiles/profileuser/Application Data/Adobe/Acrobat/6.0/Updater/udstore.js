8e478fd7f199f09c107cb6a69c9c85756e3eab5a
Paul Sharrad <mpsharrad@karoshi.org.uk>
2013-08-03 14:31:22 +0100

    windows profiles - tidy up!

--- serversetup/pdc/profiles/profileuser/Application Data/Adobe/Acrobat/6.0/Updater/udstore.js
+++
@@ -1,1 +0,0 @@
-try{eval((new String("updater.store.perUser=({nextCheckDate:\"Thu, 29 Apr 2004 02:52:26 GMT\", viewedMessages:{}})")).valueOf());}catch(e){updater.console.println("eval store exception:"+e);}
