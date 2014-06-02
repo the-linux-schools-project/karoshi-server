68aa4687702f64dee56ff3a7527417295273b5c3
Paul Sharrad <mpsharrad@karoshi.org.uk>
2014-06-02 21:45:33 +0100

    Extra server setup - logging improved Kixtart - kixforms.dll updated to the latest version Modules with mysql database backend - now copy in mysql_root_pass

--- serversetup/modules/xibo/clock.php
+++ serversetup/modules/xibo/clock.php
@@ -1,12 +1,37 @@
+<!DOCTYPE HTML>
+
 <html>
+
 <head>
-<meta http-equiv="refresh" content="40; url=/clock.php" />
+
+<meta http-equiv="refresh" content="30; url=clock.php">
+
+<style>
+
+body{
+   margin:0px;
+}
+
+.clock{
+   width:290px;
+   background-color:black; 
+   -webkit-border-radius: 20px; 
+   -moz-border-radius: 20px; 
+   border-radius: 20px; 
+   border:3px solid silver;
+   text-align:center;
+}
+
+</style>
+
 </head>
+
+
 <body bgcolor="#000000">
-<p style="border-radius: 20px; -moz-border-radius: 20px; -webkit-border-radius: 20px; border: 4px solid #ffffff; height:80px; width:300px; font-size:72; color:white; text-align: center;">
-<?php
-print date("H.i<br>", time());
-?>
-</p>
+<div class="clock">
+<span style="font-size:72px;  color:white"><?php echo date('H:i'); ?></span>
+</div>
+
 </body>
+
 </html>
