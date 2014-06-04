994a1d3203abbf2ee9e379cd67d551272c15d76e
Paul Sharrad <mpsharrad@karoshi.org.uk>
2014-06-04 20:21:19 +0100

    kixstart_install.bat - modified to work with windows 8.1

--- serversetup/pdc/logonscripts/kixtart_install.bat
+++ serversetup/pdc/logonscripts/kixtart_install.bat
@@ -1,10 +1,7 @@
 C:
 CD \
-MD C:\"Program Files"\kixtest
-if not exist C:\"Program Files"\kixtest goto permerror
-rmdir C:\"Program Files"\kixtest
 MD C:\kix
-if not exist C:\kix goto permerror2
+if not exist C:\kix goto createerror1
 COPY %LOGONSERVER%\netlogon\kix\*.* C:\kix
 
 attrib +R +S c:\kix\*.*
@@ -13,12 +10,7 @@ regsvr32 "C:\kix\kixforms.dll"
 C:\kix\WKIX32.EXE %LOGONSERVER%\netlogon\kix\WSsetup.kix
 %LOGONSERVER%\netlogon\getdll.bat
 exit
-:permerror
-echo You do not have enough permissions to install Kixtart.
-echo Please contact your network administrator.
-logoff
-exit
-:permerror2
+:createerror1
 echo Error creating C:\kix
 echo Please create the kix folder manually or change your permissions.
 echo Please logout and back in once you have done this.
